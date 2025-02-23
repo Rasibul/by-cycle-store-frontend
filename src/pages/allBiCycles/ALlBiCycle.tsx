88b5ff03c1d88a918ad9611c87ac8418f628dc5a
Rasibul <rasibul179@gmail.com>
2025-02-23 23:21:38 +0600

    updated

--- src/pages/allBiCycles/ALlBiCycle.tsx
+++ src/pages/allBiCycles/ALlBiCycle.tsx
@@ -40,7 +40,9 @@ const ALlBiCycle = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
   const [selectedType, setSelectedType] = useState("");
-  const [priceRange, setPriceRange] = useState<[number, number]>([0, 100000]);
+  const [priceRange, setPriceRange] = useState<[number, number]>([
+    15000, 50000,
+  ]);
   const [availability, setAvailability] = useState("");
 
   const filteredProducts = products.filter((product: Product) => {
@@ -120,81 +122,89 @@ const ALlBiCycle = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {data === undefined
-          ? Array.from({ length: 10 }).map((_, index) => (
-              <div
-                key={index}
-                className="bg-white p-6 rounded-xl shadow-lg flex flex-col items-center"
-              >
-                <Skeleton height={200} width={200} />
-                <Skeleton count={2} />
-                <Skeleton width={100} />
+        {data === undefined ? (
+          Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-white p-6 rounded-xl shadow-lg flex flex-col items-center"
+            >
+              <Skeleton height={200} width={200} />
+              <Skeleton count={2} />
+              <Skeleton width={100} />
+            </div>
+          ))
+        ) : filteredProducts.length === 0 ? (
+          <div className="col-span-3 text-center">
+            <p className="text-xl font-semibold text-gray-600 mt-20">
+              No products available for your search criteria.
+            </p>
+          </div>
+        ) : (
+          filteredProducts.map((product: Product) => (
+            <div
+              key={product._id}
+              className="bg-white p-6 rounded-xl shadow-lg"
+            >
+              <div className="relative overflow-hidden rounded-lg">
+                <img
+                  src={product.photo}
+                  alt={product.name}
+                  className="w-full h-56 object-cover transform hover:scale-110 transition-transform duration-300"
+                />
+                {!product.inStock && (
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <p className="text-white text-lg font-semibold">
+                      Out of Stock
+                    </p>
+                  </div>
+                )}
               </div>
-            ))
-          : filteredProducts.map((product: Product) => (
-              <div
-                key={product._id}
-                className="bg-white p-6 rounded-xl shadow-lg"
-              >
-                <div className="relative overflow-hidden rounded-lg">
-                  <img
-                    src={product.photo}
-                    alt={product.name}
-                    className="w-full h-56 object-cover transform hover:scale-110 transition-transform duration-300"
-                  />
-                  {!product.inStock && (
-                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                      <p className="text-white text-lg font-semibold">
-                        Out of Stock
-                      </p>
-                    </div>
-                  )}
-                </div>
-                <h3 className="text-2xl font-bold mt-4 text-gray-800">
-                  {product.name}
-                </h3>
-                <p className="text-gray-600 text-base mt-2">
-                  {product.description}
-                </p>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <p className="text-gray-500 text-sm font-bold">
-                      Price: {product.price} Tk
-                    </p>
-                    <p className="text-gray-500 text-sm font-bold">
-                      Brand: {product.brand}
-                    </p>
-                  </div>
-                  <div>
-                    <p className="text-gray-500 text-sm font-bold">
-                      Type: {product.type}
-                    </p>
-                    <p className="text-gray-500 text-sm font-bold">
-                      Quantity: {product.quantity}
-                    </p>
-                  </div>
-                </div>
-                <div className="mt-4 text-center">
-                  <p
-                    className={`text-lg font-medium ${
-                      product.inStock ? "text-green-600" : "text-red-600"
-                    }`}
-                  >
-                    {product.inStock ? "In Stock" : "Out of Stock"}
+              <h3 className="text-2xl font-bold mt-4 text-gray-800">
+                {product.name}
+              </h3>
+              <p className="text-gray-600 text-base mt-2">
+                {product.description}
+              </p>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <p className="text-gray-500 text-sm font-bold">
+                    Price: {product.price} Tk
+                  </p>
+                  <p className="text-gray-500 text-sm font-bold">
+                    Brand: {product.brand}
                   </p>
                 </div>
-                <button
-                  className={`mt-6 w-full px-6 py-3 rounded-lg font-semibold text-white transition-all duration-300 ${
-                    product.inStock
-                      ? "bg-blue-500 hover:bg-blue-700"
-                      : "bg-gray-400 cursor-not-allowed"
-                  }`}
-                  disabled={!product.inStock}
-                >
-                  {product.inStock ? "View Details" : "Unavailable"}
-                </button>
+                <div>
+                  <p className="text-gray-500 text-sm font-bold">
+                    Type: {product.type}
+                  </p>
+                  <p className="text-gray-500 text-sm font-bold">
+                    Quantity: {product.quantity}
+                  </p>
+                </div>
               </div>
-            ))}
+              <div className="mt-4 text-center">
+                <p
+                  className={`text-lg font-medium ${
+                    product.inStock ? "text-green-600" : "text-red-600"
+                  }`}
+                >
+                  {product.inStock ? "In Stock" : "Out of Stock"}
+                </p>
+              </div>
+              <button
+                className={`mt-6 w-full px-6 py-3 rounded-lg font-semibold text-white transition-all duration-300 ${
+                  product.inStock
+                    ? "bg-blue-500 hover:bg-blue-700"
+                    : "bg-gray-400 cursor-not-allowed"
+                }`}
+                disabled={!product.inStock}
+              >
+                {product.inStock ? "View Details" : "Unavailable"}
+              </button>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
